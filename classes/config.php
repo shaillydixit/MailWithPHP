eb4e484775febbe16e8cd674b449d1d38d145e4f
ShaillyDixitDenchInfotech <developer@denchinfotech.in>
2024-09-09 04:54:17 +0530

    config file created and connected to index and to, cc, bcc mail created

---
+++ classes/config.php
@@ -0,0 +1,33 @@
+<?php
+
+class Config
+{
+
+    /**
+     * SMTP host
+     *
+     * @var string
+     */
+    const SMTP_HOST = 'sandbox.smtp.mailtrap.io';
+
+    /**
+     * SMTP port
+     *
+     * @var int
+     */
+    const SMTP_PORT = 2525;
+
+    /**
+     * SMTP user
+     *
+     * @var string
+     */
+    const SMTP_USER = '44409837e94694';
+
+    /**
+     * SMTP password
+     *
+     * @var string
+     */
+    const SMTP_PASSWORD = '2ff6f68b6ebe22';
+}
--- index.php
+++ index.php
@@ -3,22 +3,23 @@
 use PHPMailer\PHPMailer\PHPMailer;
 
 require 'vendor/autoload.php';
+require 'classes/config.php';
 
 
 $mail = new PHPMailer();
 
 $mail->isSMTP();
-$mail->Host = 'sandbox.smtp.mailtrap.io';
+$mail->Host = Config::SMTP_HOST;
 $mail->SMTPAuth = true;
-$mail->Port = 2525;
-$mail->Username = '44409837e94694';
-$mail->Password = '2ff6f68b6ebe22';
+$mail->Port = Config::SMTP_PORT;
+$mail->Username = Config::SMTP_USER;
+$mail->Password = Config::SMTP_PASSWORD;
 
 
-/**
- * Enable SMTP debug messages
- */
-$mail->SMTPDebug = 2;
+// /**
+//  * Enable SMTP debug messages
+//  */
+// $mail->SMTPDebug = 2;
 
 
 /**
@@ -26,6 +27,22 @@ $mail->SMTPDebug = 2;
  */
 $mail->setFrom('sender@example.com');
 $mail->addAddress('recipient@example.com');
+
+
+// Multiple "To" addresses
+$mail->addAddress('alice@example.com', 'Alice');
+$mail->addAddress('alan@example.com');
+
+// "Cc" addresses
+$mail->addCC('colin@example.com', 'Colin');
+$mail->addCC('chris@example.com');
+
+// "Bcc" addresses
+$mail->addBCC('bob@example.com');
+$mail->addBCC('becky@example.com');
+
+
+$mail->Subject = 'An email sent from PHP';
 $mail->Body = 'This is a test message';
 
 if ($mail->send()) {
